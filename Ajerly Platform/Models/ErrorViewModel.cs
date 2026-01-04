1e60184940e8d921ba215dfa8d55f4b6c2c8497f
farhanalanzi <139543574+farhanalanzi@users.noreply.github.com>
2026-01-04 18:30:10 +0300

    Add full project files

---
+++ Ajerly Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ajerly_Platform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
