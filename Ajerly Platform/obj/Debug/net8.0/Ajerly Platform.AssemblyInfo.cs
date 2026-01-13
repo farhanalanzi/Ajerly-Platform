98b731bf1a85f9eafd55b9add24d6da43a687979
farhanalanzi <139543574+farhanalanzi@users.noreply.github.com>
2026-01-13 16:49:01 +0300

    Updated Card Details

--- Ajerly Platform/obj/Debug/net8.0/Ajerly Platform.AssemblyInfo.cs
+++ Ajerly Platform/obj/Debug/net8.0/Ajerly Platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ajerly Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4eb58be585ef51176906a11cdf98e57eb82ce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c8b58809e33d20484b8025d1cc6533da5835c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ajerly Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ajerly Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
